54c78cc4655f61a2f114db94dc49b11397978c6d
Paul SEREN-ROSSO <p.serenrosso@rubika-edu.com>
2022-12-09 13:08:04 +0100

    Commit tempo create active boost speed capacity

---
+++ MOBA/Assets/Scripts/Capacities/Active Capacities/ActiveBoostSpeedSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Entities.Capacities;
+using UnityEngine;
+
+public class ActiveBoostSpeedSO : ActiveCapacity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
