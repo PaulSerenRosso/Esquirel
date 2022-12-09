fb766d17ee9f9b11fe54f7d6e5bc7f351d18d549
Paul SEREN-ROSSO <p.serenrosso@rubika-edu.com>
2022-12-09 19:10:28 +0100

    Create Generic ActiveAttackCapacity

---
+++ MOBA/Assets/Scripts/Capacities/Active Capacities/SwordAttack/AttackCollider.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entities.Capacities
+{
+    
+public class AttackCollider :ActiveCapacityCollider
+{
+    
+    public override void CollideWithEntity(Entity entityCollided)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void InitCapacityCollider(ActiveCapacity activeCapacity)
+    {
+        throw new System.NotImplementedException();
+    }
+    
+
+}
+}
