5fd02398e9100599e54bb60a57d50c290596849d
BaptisteV <86929180+MadBrosos@users.noreply.github.com>
2022-12-12 19:17:05 +0100

    Fix_BakeVertex

--- MOBA/Assets/Scripts/Tools/Bake Vertex Color/Editor/BakeVertexEditor.cs
+++ MOBA/Assets/Scripts/Tools/Bake Vertex Color/Editor/BakeVertexEditor.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 [CustomEditor(typeof(BakeVertexColorMap))]
 public class BakeVertexEditor : Editor
 {
+    float secs = 4f;
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
         BakeVertexColorMap bakeVertexColorMap = (BakeVertexColorMap)target;
         if (GUILayout.Button("Bake Vertex Color As PNG"))
         {
+            var step = 0.1f;
             bakeVertexColorMap.GetMesh();
-            bakeVertexColorMap.BakeVertexColor();
+            for (float t = 0; t < secs; t += step)
+            {
+                bakeVertexColorMap.BakeVertexColor();            
+            }
         }
     }
 }
