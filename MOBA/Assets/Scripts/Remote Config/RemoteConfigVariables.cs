39dbc8d95612966ee46afc3b7491b4bfa87851e0
Pierkot <yoann59491@yahoo.fr>
2022-12-13 17:37:33 +0100

    Update remote

--- MOBA/Assets/Scripts/Remote Config/RemoteConfigVariables.cs
+++ MOBA/Assets/Scripts/Remote Config/RemoteConfigVariables.cs
@@ -1,19 +1,28 @@
 using CapturePoint;
+using Entities.Capacities;
 using Entities.Champion;
 using Entities.FogOfWar;
+using RessourceProduction;
 using UnityEngine;
 
 namespace RemoteConfig {
     public class RemoteConfigVariables : MonoBehaviour {
-        [Header("Capture Point")] public CapturePointSO generatorCapturePoint = null;
+        [Header("Capture Point")] 
+        public CapturePointSO generatorCapturePoint = null;
         public CapturePointSO relaiCapturePoint = null;
+        public GoldProductionSO relaiProduction = null;
+        public VictoryProductionSO generatorProduction = null;
         
-        [Space(8), Header("Champion 01")] 
+        [Space(8), Header("Champion 01")]
         public ChampionSO CHAMP01_BaseVariables = null;
         public ActiveAutoAttackSO CHAMP01_AutoAttack = null;
+        public TpCapacitySO CHAMP01_CAPA01 = null;
+        public SwordAttackSO CHAMP01_CAPA02 = null;
 
-        [Header("Champion 02")] 
+        [Header("Champion 02")]
         public ChampionSO CHAMP02_BaseVariables = null;
         public ActiveAutoAttackSO CHAMP02_AutoAttack = null;
+        public TailAttackSO CHAMP02_CAPA01 = null;
+        public TailAttackSO CHAMP02_CAPA02 = null;
     }
 }
