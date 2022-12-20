4e1cbcdfc90f4eb1bf25a663739b43660b6ccb11
Paul Seren-Rosso <p.serenrosso@rubika-edu.com>
2022-12-20 15:54:13 +0100

    create static icons

--- MOBA/Assets/Editor/MiniMap/MiniMapManagerEditor.cs
+++ MOBA/Assets/Editor/MiniMap/MiniMapManagerEditor.cs
@@ -2,25 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using MiniMap;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CustomEditor(typeof(MiniMapManager))]
 public class MiniMapManagerEditor : Editor
 {
     private MiniMapManager miniMapManager;
+
     private void OnEnable()
     {
         miniMapManager = (MiniMapManager)target;
-       
     }
 
+    
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        if (GUILayout.Button("Bake Static Icons"))
+        if (GUILayout.Button("Update Mini Map Pos"))
         {
-            miniMapManager.BakeStaticIcons();
+            miniMapManager.UpdateMiniMapPos();
+            EditorUtility.SetDirty(miniMapManager);
+            EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+     
         }
-        miniMapManager.UpdateMiniMapPos();
     }
 }
--- MOBA/Assets/Scripts/Mini map/MiniMapManager.cs
+++ MOBA/Assets/Scripts/Mini map/MiniMapManager.cs
@@ -10,48 +10,50 @@ namespace MiniMap
         [SerializeField] private Vector3 worldMapCenter;
         [SerializeField] Vector2 worldMapSize;
         [SerializeField] float screenMapSize;
-        [SerializeField] RectTransform mapObject;
+        public RectTransform mapObject;
 
+      
+        [SerializeField] private  Vector2 maxWorldMapPosition;
         [SerializeField] private float ratioXY;
         [SerializeField] private float ratioYX;
         [SerializeField] private Vector2 screenMapCenter;
-
-      
-        
-          private bool updateMiniMapPos;
+        [SerializeField] Vector2 minWorldMapPosition;
+        public GameObject iconPrefab;
+        public static MiniMapManager instance;
+        private void Awake()
+        {
+            instance = this;
+        }
+   // TODO curseur à corriger
+        private bool updateMiniMapPos;
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.blue;
             Gizmos.DrawWireCube(worldMapCenter, new Vector3(worldMapSize.x , 5, worldMapSize.y));
         }
 
-        private void Start()
-        {
-            UpdateMiniMapPos();
-        }
-
-        private void OnValidate()
-        {
-            updateMiniMapPos = true;
-          
-        }
+        
 
         public void UpdateMiniMapPos()
         {
-            if(!updateMiniMapPos) return;
-            updateMiniMapPos = false;
+           
+         
             ratioXY = worldMapSize.x / worldMapSize.y;
             ratioYX = worldMapSize.y / worldMapSize.x;
             mapObject.anchoredPosition = screenMapCenter;
             mapObject.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, ratioXY*screenMapSize);
             mapObject.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, ratioYX*screenMapSize);
+            minWorldMapPosition =  new Vector2(worldMapCenter.x, worldMapCenter.z) - worldMapSize/2;
+            maxWorldMapPosition = (new Vector2(worldMapCenter.x, worldMapCenter.z) + worldMapSize/2);
         }
-        
 
-        public void BakeStaticIcons()
+
+        public Vector2 GetMiniMapPos(Vector2 worldPos)
         {
-            
+            Vector2 ratioPos = ((worldPos)-minWorldMapPosition)/(maxWorldMapPosition-minWorldMapPosition);
+            return (ratioPos-Vector2.one/2)*screenMapSize;
         }
+       
        
        
     }
